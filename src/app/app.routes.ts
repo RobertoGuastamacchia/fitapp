7859aced55e3db5ff4f77522d6e40517b4022b2a
RobertoGuastamacchia <roberto.guastamacchia@external.eni.com>
2024-01-17 19:23:59 +0100

    unifit v0.0

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { NgModule } from '@angular/core';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    { path: 'login', component: LoginComponent},    
+    { path: '', component: LoginComponent},
+    
+  ];
+NgModule({
+    imports: [RouterModule.forRoot(routes,{useHash: true})],
+    exports: [RouterModule],
+    
+  })
